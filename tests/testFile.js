cb7fc4b54fec7c8f4d82e87fa557d15b05223f70
Sina2192 <Sina2192@yahoo.com>
2020-10-07 00:19:59 -0500

    Initial tests added

--- tests/testFile.js
+++ tests/testFile.js
@@ -1,7 +1,40 @@
-import DB from "/Users/sinamb/Documents/Programs/SinaService/database/index.js";
-import server from "/Users/sinamb/Documents/Programs/SinaService/server/index.js";
-import client from "/Users/sinamb/Documents/Programs/SinaService/client/source/index.jsx";
-//entering an invalid number
-//there are 100 entries
-//is able to fetch entry 99
-test ()
+// import DB from "../database/index";
+// import server from "../server/index";
+// import client from "../client/source/index";
+var req = require("supertest")
+
+
+describe('API communication: ', () => {
+  var app = req('http://localhost:3007');
+
+  describe('GET requests to /updates/:projectId', () => {
+
+    test('The database should get seeded with 100 entries', () => {
+      app.get('/updates/100')
+        .then(response => {
+          expect(response.status).toBe(200);
+        })
+    });
+
+    test('Returns a 200 response for valid projectIDs', () => {
+      app.get('/updates/99')
+        .then(response => {
+          expect(response.status).toBe(200);
+        })
+    });
+
+    test('The response contains a pledgeAmount that is 100 points above the projectId', () => {
+      app.get('/updates/5')
+        .then(response => {
+          expect(response.body.pledgeAmount).toEqual(105);
+        })
+    });
+
+    test('400 error for invalid projectIds', () => {
+      app.get('/updates/101')
+        .then(response => {
+          expect(response.status).toBe(400);
+        })
+    });
+  });
+});
